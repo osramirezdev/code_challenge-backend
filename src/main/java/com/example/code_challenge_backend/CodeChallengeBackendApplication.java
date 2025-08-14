0cfa58f14b72373809a357d424b688717af9839d
osramirezdev <osramirezdev@gmail.com>
2025-08-14 13:47:00 -0300

    initial commit backend

---
+++ src/main/java/com/example/code_challenge_backend/CodeChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.code_challenge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/code_challenge_backend/CodeChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_challenge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
